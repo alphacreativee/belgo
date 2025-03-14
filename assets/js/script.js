f9ca6820dfbc83e8e6cae6d4c3d780b492c70cc6
HuuBang11 <huubang0312@gmail.com>
2025-03-14 10:44:42 +0700

    update footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,58 @@ import { preloadImages } from "../libs/utils.js";
 let lenis;
 Splitting();
 
-const init = () => {};
+function footer() {
+  const footerElement = document.querySelector("footer");
+  const footerTop = document.querySelector(".footer-top");
+  const footerTopHeight = footerTop.offsetHeight;
+  const footerOvl = document.querySelector(".footer-ovl");
+
+  gsap.to("footer", {
+    scrollTrigger: {
+      trigger: "footer",
+      start: "top top",
+      end: () => `+=${footerTopHeight * 2}`,
+      scrub: 1,
+      pin: true,
+      // markers: true,
+      onUpdate: (self) => {
+        const progress = self.progress;
+        if (progress >= 0.6) {
+          footerElement.classList.add("done");
+        } else {
+          footerElement.classList.remove("done");
+        }
+      }
+    }
+  });
+
+  gsap.fromTo(
+    footerOvl,
+    {
+      transform: "skewY(-30deg) translateY(-150%)"
+    },
+    {
+      transform: "skewY(-30deg) translateY(100%)",
+      scrollTrigger: {
+        trigger: "footer",
+        start: "top top",
+        end: () => `+=${footerTopHeight * 2}`,
+        scrub: 1
+        // markers: { startColor: "green", endColor: "red" }
+      },
+      ease: "none"
+    }
+  );
+
+  window.addEventListener("load", () => {
+    ScrollTrigger.refresh();
+  });
+}
+
+const init = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  footer();
+};
 preloadImages("img").then(() => {
   // Once images are preloaded, remove the 'loading' indicator/class from the body
 
